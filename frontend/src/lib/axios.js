cd208c3afdc0efdf8783a9e7a327af7b7808a37d
Balaji091 <chokkarabalaji@gmail.com>
2025-03-05 15:17:04 +0530

    url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/" : "/api",
+  baseURL: import.meta.env.NODE_ENV=development
+  === "development" ? "http://localhost:5000/" : "/api",
   withCredentials: true,
 });
